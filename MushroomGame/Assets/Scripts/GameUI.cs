127cf6564d78302bd92df8f04851cccc8327deb6
Miggie <109484716+miggiemadz@users.noreply.github.com>
2023-10-01 19:17:51 -0400

    Health Started

--- MushroomGame/Assets/Scripts/GameUI.cs
+++ MushroomGame/Assets/Scripts/GameUI.cs
@@ -12,6 +12,9 @@ public class GameUI : MonoBehaviour
     public TMP_Text acornCountText;
     public int currentAcorns = 0;
 
+    public Image[] healthMushrooms;
+    public int currentHealth;
+
     void Awake()
     {
         instance = this; 
@@ -20,11 +23,48 @@ public class GameUI : MonoBehaviour
     void Start()
     {
         acornCountText.text = "x" + currentAcorns.ToString();
+        currentHealth = 3;
     }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ManageHealth(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ManageHealth(-1);
+        }
+
+        Debug.Log(currentHealth);
+    }
+
 
     public void IncreaseAcorns(int value)
     {
         currentAcorns += value;
         acornCountText.text = "x" + currentAcorns.ToString();
+    }
+
+    public void ManageHealth(int value)
+    {
+        currentHealth -= value;
+
+        if (currentHealth >= 0 && currentHealth <= 3)
+        {
+            for (int i = 0; i < healthMushrooms.Length; i++)
+            {
+                if (i < currentHealth)
+                {
+                    healthMushrooms[i].color = Color.white;
+                }
+                else
+                {
+                    healthMushrooms[i].color = Color.black;
+                }
+            }
+        }
     }
 }
---
+++ MushroomGame/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+
+    public GameObject Noki;
+    public int damage;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
