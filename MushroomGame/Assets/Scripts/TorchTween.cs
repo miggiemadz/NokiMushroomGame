7f216984f794a1b32f2fc4624533a8b2e65effc4
Miggie <109484716+miggiemadz@users.noreply.github.com>
2023-10-31 22:19:30 -0400

    Torches + Light Animation

--- MushroomGame/Assets/Scripts/TorchTween.cs
+++ MushroomGame/Assets/Scripts/TorchTween.cs
@@ -8,27 +8,50 @@ public class TorchTween : MonoBehaviour
 {
     [SerializeField] private Light2D torchLight;
 
-    private float lightRadiusSmall = .35f;
-    private float lightRadiusLarge = 2f;
-    [SerializeField] private float bloomTime;
-    private float lightStrength;
-    private float torchIntensity;
+    private bool isIncreasing = true;
+    private float timeElapsed;
+    private float duration = 2f;
+    [SerializeField] private float lightRadiusSmall = .35f;
+    [SerializeField] private float lightRadiusLarge = 2.0f;
 
     void Start()
     {
-        torchLight.intensity = lightRadiusSmall;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Tween();
+        if (isIncreasing)
+        {
+            if (timeElapsed < duration)
+            {
+                torchLight.intensity = Mathf.Lerp(lightRadiusSmall, lightRadiusLarge, timeElapsed / duration);
+                timeElapsed += Time.deltaTime;
+            }
+            else
+            {
+                torchLight.intensity = lightRadiusLarge;
+                isIncreasing = !isIncreasing;
+                timeElapsed = 0;
+            }
+        }
 
-    }
+        if (!isIncreasing)
+        {
+            if (timeElapsed < duration)
+            {
+                torchLight.intensity = Mathf.Lerp(lightRadiusLarge, lightRadiusSmall, timeElapsed / duration);
+                timeElapsed += Time.deltaTime;
+            }
+            else
+            {
+                torchLight.intensity = lightRadiusSmall;
+                isIncreasing = !isIncreasing;
+                timeElapsed = 0;
+            }
+        }
 
-    private void Tween()
-    {
-        LeanTween.value(lightRadiusSmall, lightRadiusLarge, bloomTime);
+        Debug.Log(isIncreasing + ", " + timeElapsed);
     }
 }
